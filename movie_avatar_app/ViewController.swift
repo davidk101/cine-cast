55e9aeea3ad1089107c017af9db8882e1462713c
David Kumar <david.dn.kumar@gmail.com>
2021-03-18 15:02:25 -0500

    Initial Commit

---
+++ movie_avatar_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movie_avatar_app
+//
+//  Created by David Kumar on 3/18/21.
+//  Copyright © 2021 David Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
